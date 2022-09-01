e71045b8c662cacb9a33988cf1f3ef83f13c6180
Konrad Chwesiuk <46359181+FezMLG@users.noreply.github.com>
2022-09-01 21:34:57 +0200

    update: rest client

--- src/api/rest/RestClient.ts
+++ src/api/rest/RestClient.ts
@@ -1,14 +1,25 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
 export class RestClient {
-  constructor(private baseUrl: string) {}
+  private instance: AxiosInstance;
 
-  async get<T>(): Promise<T> {
-    const { data, status } = await axios.get<T>(this.baseUrl, {
+  constructor(private baseUrl: string) {
+    this.instance = axios.create({
+      baseURL: this.baseUrl,
+      timeout: 2000,
       headers: {
         Accept: "application/json",
       },
     });
+  }
+
+  async get<T>(): Promise<T> {
+    const { data } = await this.instance.get<T>(this.baseUrl);
+    return data;
+  }
+
+  async post<T>(dataToSend: Object): Promise<T> {
+    const { data } = await this.instance.post<T>(this.baseUrl, dataToSend);
     return data;
   }
 }
